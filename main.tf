453f21dbad7cfac797e4258ff07ba5cacec83f4b
JoelClemente <joel.lopez106@alu.ulpgc.es>
2025-01-16 23:01:08 +0000

    Hazelcast ssh

--- main.tf
+++ main.tf
@@ -61,7 +61,14 @@ resource "aws_security_group" "hazelcast_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
