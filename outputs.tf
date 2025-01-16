bdf55025c4ac916c85acc61e23f6144903825a8b
JoelClemente <joel.lopez106@alu.ulpgc.es>
2025-01-16 19:34:45 +0000

    hazelcast server

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-output "instance_id" {
-  value = aws_instance.datamart_instance.id
-  description = "ID de la instancia creada en AWS."
+output "hazelcast_public_ip" {
+  value = aws_instance.hazelcast_instance.public_ip
+  description = "Public IP of the Hazelcast server instance"
 }
 
-# Output para verificar la IP Pública
-output "datamart_public_ip" {
+output "datamart_instance_public_ip" { # Renombrado para evitar conflicto
   value = aws_instance.datamart_instance.public_ip
   description = "Public IP of the Datamart instance"
 }
-
